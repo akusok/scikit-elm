9dec2dff8a41a5ab7cee3f8a89a98a618fbab5e4
Anton Akusok <akusok.a@gmail.com>
2021-06-27 11:33:01 +0200

    Working Lanczos solver

--- skelm/solver_lanczos.py
+++ skelm/solver_lanczos.py
@@ -1,8 +1,15 @@
 import numpy as np
 from numpy.typing import ArrayLike
-from typing import Optional
+from typing import Optional, Callable, List
 
 from .solver import Solver
+
+
+def worst_of_five(errors: List[float]) -> bool:
+    """Returns true when the last error is the worst of five last ones."""
+    if len(errors) > 3 and (errors[-1] > max(errors[-5:-1])):
+        return True
+    return False
 
 
 class LanczosSolver(Solver):
@@ -14,31 +21,33 @@ class LanczosSolver(Solver):
     data_features: Optional[int] = None
     validation_features: Optional[int] = None
 
-    def fit(self, X, y, X_val=None, y_val=None):
+    def fit(self, X, y, X_val=None, y_val=None, stopping_condition: Callable[[List[float]], bool] = worst_of_five):
         """Solve system using validation set for automatic early stopping.
 
-        :param X: training inputs
-        :param y: training targets
-        :param X_val: validation inputs
-        :param y_val: validation targets
+        :param X:
+        :param y:
+        :param X_val:
+        :param y_val:
+        :param stopping_condition:  func(validation_error_list) -> bool. Function returning True to stop solution early.
+        :return:
         """
-        l = X.shape[1]
-        r = [np.nan] * (l + 1)
-        q = [np.nan] * (l + 1)
-        a = [np.nan] * (l + 1)
-        b = [np.nan] * (l + 2)
+        dim = X.shape[1]
+        r = [np.nan] * (dim + 1)
+        q = [np.nan] * (dim + 1)
+        a = [np.nan] * (dim + 1)
+        b = [np.nan] * (dim + 2)
 
         r[0] = X.T @ y
-        q[0] = np.zeros(l)
+        q[0] = np.zeros(dim)
         b[1] = np.linalg.norm(r[0])
 
-        e1 = np.zeros((l,))
+        e1 = np.zeros((dim,))
         e1[0] = 1
 
         rmse_v = []
         B = None
 
-        for j in range(1, l + 1):
+        for j in range(1, dim + 1):
             q[j] = r[j - 1] / b[j]
             M = X @ q[j]
             a[j] = M.T @ M
@@ -54,7 +63,7 @@ class LanczosSolver(Solver):
                     continue
 
                 rmse_v.append(np.mean((X_val @ B - y_val) ** 2) ** 0.5)
-                if len(rmse_v) > 3 and (rmse_v[-1] > max(rmse_v[-5:-1])):
+                if stopping_condition(rmse_v):
                     break
 
         self.coef_ = B
--- skelm/tests/test_solvers.py
+++ skelm/tests/test_solvers.py
@@ -3,8 +3,85 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 from sklearn.utils.estimator_checks import check_estimator
+from sklearn.datasets import load_diabetes
 
 from skelm.solver_batch import BatchCholeskySolver
+from skelm.solver_lanczos import LanczosSolver
+
+
+class TestLanczosSolver(unittest.TestCase):
+
+    def test_SolveNoBias_Works(self):
+        X = np.random.randn(100, 3)
+        Y = X @ np.array([1, 2, 3])
+        solver = LanczosSolver().fit(X[::2], Y[::2])
+        Yh = X[1::2] @ solver.coef_
+        assert_allclose(Y[1::2], Yh, rtol=1e-3)
+        assert_allclose(solver.coef_, np.array([1, 2, 3]), rtol=1e-3)
+
+    def test_SolveArtificialIntercept_Works(self):
+        X_no_intercept = np.random.randn(100, 3)
+        Y = X_no_intercept @ np.array([1, 2, 3]) - 2
+        X = np.hstack((np.ones((100, 1)), X_no_intercept))
+        solver = LanczosSolver().fit(X[::2], Y[::2])
+        Yh = X[1::2] @ solver.coef_
+        assert_allclose(Y[1::2], Yh, rtol=1e-3)
+        assert_allclose(solver.coef_[1:], np.array([1, 2, 3]), rtol=1e-3)
+        assert_allclose(solver.coef_[:1], -2)
+
+    def test_IncrementalSoluiton_MoreIterationsDecreaseError(self):
+
+        def stop_two_iter(e):
+            return len(e) >= 2
+
+        def stop_five_iter(e):
+            return len(e) >= 5
+
+        def never_stop(_):
+            return False
+
+        X = np.random.randn(100, 10)
+        true_coef = np.random.randn(10)
+        Y = X @ true_coef
+        solver = LanczosSolver()
+
+        solver.fit(X[::2], Y[::2], X[1::2], Y[1::2], stopping_condition=stop_two_iter)
+        rmse_two_iter = np.mean((Y[1::2] - X[1::2] @ solver.coef_)**2)**0.5
+
+        solver.fit(X[::2], Y[::2], X[1::2], Y[1::2], stopping_condition=stop_five_iter)
+        rmse_five_iter = np.mean((Y[1::2] - X[1::2] @ solver.coef_)**2)**0.5
+
+        solver.fit(X[::2], Y[::2], X[1::2], Y[1::2], stopping_condition=never_stop)
+        rmse_full_solution = np.mean((Y[1::2] - X[1::2] @ solver.coef_)**2)**0.5
+
+        print(rmse_two_iter)
+        print(rmse_five_iter)
+        print(rmse_full_solution)
+
+        self.assertLess(rmse_five_iter, rmse_two_iter)
+        self.assertLess(rmse_full_solution, rmse_five_iter)
+        self.assertLess(rmse_full_solution, 1e-5)
+
+    def test_ValidationEarlyStopping_ImprovesSolution(self):
+        X, y = load_diabetes(return_X_y=True)
+
+        H = np.tanh(X @ np.random.randn(X.shape[1], 99))  # make lots of extra features
+        H = np.hstack((np.ones((H.shape[0], 1)), H))  # add bias column - Lanczos solver has no bias
+
+        Ht, Hv, Hs = H[0::3], H[1::3], H[2::3]
+        Yt, Yv, Ys = y[0::3], y[1::3], y[2::3]
+
+        def never_stop(_):
+            return False
+
+        solver = LanczosSolver()
+        solver.fit(Ht, Yt, Hv, Yv)
+        rmse_validation = np.mean((Ys - Hs @ solver.coef_)**2)**0.5
+
+        solver.fit(Ht, Yt, Hv, Yv, stopping_condition=never_stop)
+        rmse_full_solution = np.mean((Ys - Hs @ solver.coef_)**2)**0.5
+
+        self.assertLess(rmse_validation, rmse_full_solution)
 
 
 class TestCholeskySolver(unittest.TestCase):
@@ -39,7 +116,6 @@ class TestCholeskySolver(unittest.TestCase):
         Yh = solver.predict(X[1::2])
         self.assertEqual(len(Yh.shape), 1)
     
-    
     def test_OutputShape_2d(self):
         X = np.random.randn(100, 3)
         W = np.array([[1, 4],
@@ -50,7 +126,6 @@ class TestCholeskySolver(unittest.TestCase):
         solver = BatchCholeskySolver().partial_fit(X[::2], Y[::2])
         Yh = solver.predict(X[1::2])
         self.assertEqual(len(Yh.shape), 2)
-    
     
     def test_PartialFit_SeveralParts(self):
         X = np.random.randn(100, 3)
